146e23273ff688de39743d1f66cb7a7cb4d95818
bradcodecraft <bradcodecraft@gmail.com>
2025-08-29 06:40:30 +0700

    feat: Add solution to "707. Design Linked List"

--- JavaScript/src/707-design-linked-list.js
+++ JavaScript/src/707-design-linked-list.js
@@ -2,62 +2,110 @@ class Node {
   constructor(val) {
     this.val = val;
     this.next = null;
+    this.prev = null;
   }
 }
 
 class MyLinkedList {
   constructor() {
+    /** @type {Node} */
     this.head = null;
     this.size = 0;
   }
 
+  /**
+   * @param {number} index
+   * @returns {number}
+   */
   get(index) {
     if (index < 0 || index >= this.size) return -1;
-    let curr = this.head;
-    for (let i = 0; i < index; i++) curr = curr.next;
-    return curr.val;
-  }
 
-  addAtHead(val) {
-    const node = new Node(val);
-    node.next = this.head;
-    this.head = node;
-    this.size++;
-  }
+    let current = this.head;
 
-  addAtTail(val) {
-    const node = new Node(val);
-    if (!this.head) {
-      this.head = node;
-    } else {
-      let curr = this.head;
-      while (curr.next) curr = curr.next;
-      curr.next = node;
+    while (index > 0) {
+      current = current.next;
+      index -= 1;
     }
-    this.size++;
+
+    return current.val;
   }
 
+  /**
+   * @param {number} val
+   */
+  addAtHead(val) {
+    const newNode = new Node(val);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.size += 1;
+  }
+
+  /**
+   * @param {number} val
+   */
+  addAtTail(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      newNode.prev = current;
+      current.next = newNode;
+    }
+
+    this.size += 1;
+  }
+
+  /**
+   * @param {number} index
+   * @param {number} val
+   */
   addAtIndex(index, val) {
     if (index < 0 || index > this.size) return;
     if (index === 0) return this.addAtHead(val);
-    let curr = this.head;
-    for (let i = 0; i < index - 1; i++) curr = curr.next;
-    const node = new Node(val);
-    node.next = curr.next;
-    curr.next = node;
-    this.size++;
+
+    let current = this.head;
+    let pointer = 0;
+
+    while (pointer < index - 1) {
+      current = current.next;
+      pointer += 1;
+    }
+
+    const newNode = new Node(val);
+    newNode.prev = current;
+    newNode.next = current.next;
+    current.next = newNode;
+    this.size += 1;
   }
 
+  /**
+   * @param {number} index
+   */
   deleteAtIndex(index) {
     if (index < 0 || index >= this.size) return;
+
     if (index === 0) {
       this.head = this.head.next;
     } else {
-      let curr = this.head;
-      for (let i = 0; i < index - 1; i++) curr = curr.next;
-      curr.next = curr.next.next;
+      let current = this.head;
+      let pointer = 0;
+
+      while (pointer < index - 1) {
+        current = current.next;
+        pointer += 1;
+      }
+
+      current.next = current.next.next;
     }
-    this.size--;
+
+    this.size -= 1;
   }
 }
 
--- JavaScript/test/707-design-linked-list.test.js
+++ JavaScript/test/707-design-linked-list.test.js
@@ -1,107 +1,44 @@
-// import { expect } from "chai";
-// import { beforeEach, describe, test } from "mocha";
-// import { MyLinkedList } from "../src/707-design-linked-list.js";
-//
-// describe("should create a MyLinkedList instance", () => {
-//   test("new MyLinkedList()", () => {
-//     const myLinkedList = new MyLinkedList();
-//
-//     expect(() => myLinkedList).to.not.throw();
-//     expect(myLinkedList.value).to.be.undefined;
-//     expect(myLinkedList.next).to.be.undefined;
-//   });
-// });
-//
-// describe("methods should work", () => {
-//   /** @type {MyLinkedList} */
-//   let myLinkedList = null;
-//   beforeEach(() => {
-//     myLinkedList = new MyLinkedList();
-//   });
-//
-//   test("addToHead()", () => {
-//     myLinkedList.addAtHead(1);
-//
-//     expect(myLinkedList.head.value).to.equal(1);
-//
-//     myLinkedList.addAtHead(2);
-//
-//     expect(myLinkedList.head.value).to.equal(2);
-//     expect(myLinkedList.head.next.value).to.equal(1);
-//   });
-//
-//   test("addAtTail()", () => {
-//     myLinkedList.addAtTail(1);
-//
-//     expect(myLinkedList.value).to.equal(1);
-//
-//     myLinkedList.addAtTail(2);
-//
-//     expect(myLinkedList.value).to.equal(1);
-//     expect(myLinkedList.next.value).to.equal(2);
-//   });
-//
-//   test("addAtIndex()", () => {
-//     myLinkedList.addAtIndex(0, 1);
-//
-//     expect(myLinkedList.value).to.equal(1);
-//
-//     myLinkedList.addAtIndex(0, 2);
-//
-//     expect(myLinkedList.value).to.equal(2);
-//     expect(myLinkedList.next.value).to.equal(1);
-//
-//     myLinkedList.addAtIndex(2, 3);
-//
-//     expect(myLinkedList.value).to.equal(2);
-//     expect(myLinkedList.next.value).to.equal(1);
-//     expect(myLinkedList.next.next.value).to.equal(3);
-//
-//     myLinkedList.addAtIndex(1, 0);
-//
-//     expect(myLinkedList.value).to.equal(2);
-//     expect(myLinkedList.next.value).to.equal(0);
-//     expect(myLinkedList.next.next.value).to.equal(1);
-//     expect(myLinkedList.next.next.next.value).to.equal(3);
-//   });
-//
-//   test("deleteAtIndex()", () => {
-//     myLinkedList.addAtHead(1);
-//     myLinkedList.addAtHead(2);
-//     myLinkedList.addAtIndex(2, 3);
-//     myLinkedList.addAtIndex(1, 0);
-//
-//     expect(myLinkedList.value).to.equal(2);
-//     expect(myLinkedList.next.value).to.equal(0);
-//     expect(myLinkedList.next.next.value).to.equal(1);
-//     expect(myLinkedList.next.next.next.value).to.equal(3);
-//
-//     myLinkedList.deleteAtIndex(0);
-//
-//     expect(myLinkedList.value).to.equal(0);
-//     expect(myLinkedList.next.value).to.equal(1);
-//     expect(myLinkedList.next.next.value).to.equal(3);
-//
-//     myLinkedList.deleteAtIndex(2);
-//
-//     expect(myLinkedList.value).to.equal(0);
-//     expect(myLinkedList.next.value).to.equal(1);
-//
-//     myLinkedList.deleteAtIndex(2);
-//
-//     expect(myLinkedList.value).to.equal(0);
-//     expect(myLinkedList.next.value).to.equal(1);
-//   });
-// });
-//
-// describe("leetcode", () => {
-//   test("case 1", () => {
-//     const myLinkedList = new MyLinkedList();
-//     myLinkedList.addAtHead(1);
-//     myLinkedList.addAtTail(3);
-//     myLinkedList.addAtIndex(1, 2);
-//     expect(myLinkedList.get(1)).to.equal(2);
-//     myLinkedList.deleteAtIndex(1);
-//     expect(myLinkedList.get(1)).to.equal(3);
-//   });
-// });
+import { expect } from "chai";
+import { beforeEach, describe, test } from "mocha";
+import { Node, MyLinkedList } from "../src/707-design-linked-list.js";
+
+describe("Design Linked List", () => {
+  test("Case 1", () => {
+    let myLinkedList = new MyLinkedList();
+    myLinkedList.addAtHead(1);
+    let expected = new MyLinkedList();
+    expected.head = new Node(1);
+    expected.size = 1;
+
+    expect(myLinkedList).to.deep.equal(expected);
+
+    myLinkedList.addAtTail(3);
+    expected.head.next = new Node(3);
+    expected.head.next.prev = expected.head;
+    expected.size = 2;
+
+    expect(myLinkedList).to.deep.equal(expected);
+
+    myLinkedList.addAtIndex(1, 2); // linked list becomes 1->2->3
+    expected = new MyLinkedList();
+    expected.head = new Node(1);
+    expected.head.next = new Node(2);
+    expected.head.next.prev = expected.head;
+    expected.head.next.next = new Node(3);
+    expected.head.next.next.prev = expected.head.next;
+    expected.size = 3;
+
+    expect(myLinkedList.get(1)).to.equal(2); // return 2
+
+    myLinkedList.deleteAtIndex(1); // now the linked list is 1->3
+    expected = new MyLinkedList();
+    expected.head = new Node(1);
+    expected.head.next = new Node(3);
+    expected.head.next.prev = expected.head;
+    expected.size = 2;
+
+    expect(myLinkedList).to.deep.equal(expected);
+
+    expect(myLinkedList.get(1)).to.equal(3); // return 3;
+  });
+});
