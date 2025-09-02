5fb136ffd8261c66e01345de1ed8722ceaff0a32
bradcodecraft <bradcodecraft@gmail.com>
2025-09-02 14:09:04 +0700

    feat: Add solution to "747. Largest Number At Least Twice of Others"

--- Java/src/main/java/dev/Solution.java
+++ Java/src/main/java/dev/Solution.java
@@ -9,17 +9,37 @@ public class Solution {
     int total = 0;
 
     for (int num : nums) {
-      total += num; 
+      total += num;
     }
 
     for (int i = 0; i < nums.length; i++) {
       if (left * 2 == total - nums[i]) {
         return i;
-      } 
+      }
 
       left += nums[i];
     }
 
     return -1;
+  }
+
+  public int dominateIndex(int[] nums) {
+    int max = 0;
+    int maxIndex = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] > max) {
+        max = nums[i];
+        maxIndex = i;
+      }
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] * 2 > max && i != maxIndex) {
+        return -1;
+      }
+    }
+
+    return maxIndex;
   }
 }
--- Java/src/test/java/dev/SolutionTest.java
+++ Java/src/test/java/dev/SolutionTest.java
@@ -13,15 +13,24 @@ public class SolutionTest {
   @Test
   public void pivotIndexTest() {
     int[] caseOne = {1, 7, 3, 6, 5, 6};
-    assertEquals(this.solution.pivotIndex(caseOne), 3);  
+    assertEquals(this.solution.pivotIndex(caseOne), 3);
 
     int[] caseTwo = {1, 2, 3};
-    assertEquals(this.solution.pivotIndex(caseTwo), -1);  
+    assertEquals(this.solution.pivotIndex(caseTwo), -1);
 
     int[] caseThree = {2, 1, -1};
     assertEquals(this.solution.pivotIndex(caseThree), 0);
 
     int[] caseFour = {0, 0};
     assertEquals(this.solution.pivotIndex(caseFour), 0);
+  }
+
+  @Test
+  public void dominantIndexTest() {
+    int[] caseOne = {3, 6, 1, 0};
+    assertEquals(solution.dominateIndex(caseOne), 1);
+
+    int[] caseTwo = {1, 2, 3, 4};
+    assertEquals(solution.dominateIndex(caseTwo), -1);
   }
 }
