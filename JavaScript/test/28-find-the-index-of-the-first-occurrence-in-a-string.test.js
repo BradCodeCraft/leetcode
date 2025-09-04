5191245833420cfef9f48c1794f93282a970564a
bradcodecraft <bradcodecraft@gmail.com>
2025-09-04 14:20:46 +0700

    feat: Add solution to "28. Find the Index of the First Occurrence in a String"

--- JavaScript/test/28-find-the-index-of-the-first-occurrence-in-a-string.test.js
+++ JavaScript/test/28-find-the-index-of-the-first-occurrence-in-a-string.test.js
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { describe, test } from "mocha";
 import { strStr } from "../src/28-find-the-index-of-the-first-occurrence-in-a-string.js";
 
-describe("Fidn the Index of the First Occurrence in a String", () => {
+describe("Find the Index of the First Occurrence in a String", () => {
   test("Case 1", () => {
     expect(strStr("sadbutsad", "sad")).to.eq(0);
   });
